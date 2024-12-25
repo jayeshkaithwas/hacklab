37198f90386f00dddf16c1a2504e10f508224d50
jayeshkaithwas <jayuukaithwas@gmail.com>
2024-12-25 21:45:57 +0530

    Quartz sync: Dec 25, 2024, 9:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Blogs",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
