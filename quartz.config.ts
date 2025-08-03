fdfe182985041114cdc2154607a160ca9859f2d5
jayeshkaithwas <jayuukaithwas@gmail.com>
2025-08-03 09:58:58 +0530

    Quartz sync: Aug 3, 2025, 9:58 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 HackLab",
+    pageTitle: "HackLab",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
